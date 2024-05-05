b7572d895e87a0d23f3a35971d1529a1e705392d
jwpark1211 <1211abc@naver.com>
2024-05-05 20:49:48 +0900

    feat: init bookitty

---
+++ src/main/java/capstone/bookitty/BookittyApplication.java
@@ -0,0 +1,13 @@
+package capstone.bookitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookittyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capstone/bookitty/BookittyApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.bookitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
