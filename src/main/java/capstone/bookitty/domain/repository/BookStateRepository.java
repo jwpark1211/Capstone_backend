3acb5440f43e672acaa775fa0cf0a4060908cd13
jwpark1211 <1211abc@naver.com>
2024-05-10 23:17:08 +0900

    feat : develop bookState repo

--- src/main/java/capstone/bookitty/domain/repository/BookStateRepository.java
+++ src/main/java/capstone/bookitty/domain/repository/BookStateRepository.java
@@ -4,7 +4,11 @@ import capstone.bookitty.domain.entity.BookState;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookStateRepository extends JpaRepository<BookState,Long> {
-
+    List<BookState> findByIsbn(String isbn);
+    List<BookState> findByMemberId(Long memberId);
+    boolean existsByMemberIdAndIsbn(Long memberId, String isbn);
 }
