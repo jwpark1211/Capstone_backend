42d1230171b9b2f6551d6cc7e0de5ab15a85e4f3
jwpark1211 <1211abc@naver.com>
2024-05-13 22:50:25 +0900

    fix : fix CORS

--- src/main/java/capstone/bookitty/BookittyApplication.java
+++ src/main/java/capstone/bookitty/BookittyApplication.java
@@ -2,12 +2,26 @@ package capstone.bookitty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BookittyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookittyApplication.class, args);
+	}
+
+	//CORS 에러 해결
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
