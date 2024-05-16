0e3158f677892a655bfac7eb63151e41475208f1
jwpark1211 <1211abc@naver.com>
2024-05-16 19:12:10 +0900

    feat : comment & like_count entity

---
+++ src/main/java/capstone/bookitty/domain/entity/Comment.java
@@ -0,0 +1,46 @@
+package capstone.bookitty.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="comment")
+public class Comment {
+
+    @Id @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String isbn;
+    private String content;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt;
+    @DateTimeFormat(pattern = "yyyy-mm-dd'T'HH:mm:ss")
+    private LocalDateTime modifiedAt;
+
+    @Builder
+    public Comment(Member member, String isbn, String content) {
+        this.member = member;
+        this.isbn = isbn;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void updateContent(String content){
+        this.content = content;
+        this.modifiedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/capstone/bookitty/domain/entity/Like.java
@@ -0,0 +1,38 @@
+package capstone.bookitty.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "like_count")
+public class Like {
+
+    @Id @Column(name="like_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @DateTimeFormat(pattern = "yyyy-mm-dd'T'HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Like(Member member, Comment comment){
+        this.member = member;
+        this.comment = comment;
+        this.createdAt = LocalDateTime.now();
+    }
+}
