a26bca68f9962478515fb66e07ce97a8f39d4877
jwpark1211 <1211abc@naver.com>
2024-05-17 00:21:26 +0900

    fix : timezone setting - kst

--- src/main/java/capstone/bookitty/BookittyApplication.java
+++ src/main/java/capstone/bookitty/BookittyApplication.java
@@ -1,16 +1,26 @@
 package capstone.bookitty;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BookittyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookittyApplication.class, args);
+	}
+
+	//한국 표준 시간 설정
+	@PostConstruct
+	public void started() {
+		// timezone UTC 셋팅
+		TimeZone.setDefault(TimeZone.getTimeZone("KST"));
 	}
 
 	//CORS 에러 해결
