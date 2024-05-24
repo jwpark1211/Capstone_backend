d4db75d17802a49c7eedd9bdbdfd71723d935024
jwpark1211 <1211abc@naver.com>
2024-05-25 03:26:39 +0900

    fix : authenticated api(swagger) 2..

--- src/main/java/capstone/bookitty/common/SecurityConfig.java
+++ src/main/java/capstone/bookitty/common/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                                         antMatcher("/members/login"),
                                         antMatcher("/members/new"),
                                         antMatcher("/members/email/**"),
-                                        antMatcher("/swagger-ui/index.html")).permitAll()
+                                        antMatcher("/swagger-ui/**"),
+                                        antMatcher("/v3/**")).permitAll()
                                 //.requestMatchers(antMatcher("")).authenticated()
                                 .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
